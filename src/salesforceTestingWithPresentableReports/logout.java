519ddca5241ca3e17e903aa93c84edb961019646
abhasrambo <abhasrambo@mail.fresnostate.edu>
2017-08-29 18:18:49 -0700

    Implemented 10
    Implemented 10 cases

---
+++ src/salesforceTestingWithPresentableReports/logout.java
@@ -0,0 +1,100 @@
+package salesforceTestingWithPresentableReports;
+
+/*Author: Abhas Kumar
+ * Module Name: logout
+ * Project Description: Read test values from excel and perform Login test action and then logout 
+ * Date:August 29,2017
+ */
+
+import jxl.*;
+import jxl.read.biff.BiffException;
+
+import java.io.*;
+import java.util.Scanner;
+import org.eclipse.jetty.client.SendFailure;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class logout {
+	private static WebDriver driver;
+	@SuppressWarnings("static-access")
+	public static void login() throws IOException, InterruptedException{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter absolute path of excel file");
+		String path = sc.nextLine();
+		String objUsername = "", username = "", userNameXpath = "",objPassword = "", password = "", passwordXpath = "", objLogin = "", loginXpath = "", objUserProfile = "", userProfileID = "";
+
+		Workbook workbook = null;
+		try{
+			workbook = Workbook.getWorkbook(new File(path));
+
+			Sheet sheet = workbook.getSheet(7);
+			Cell cellOneOne = sheet.getCell(1, 1);
+			objUsername = cellOneOne.getContents();
+
+			Cell cellTwoOne = sheet.getCell(2, 1);
+			username = cellTwoOne.getContents();
+
+			Cell cellThreeOne = sheet.getCell(3, 1);
+			userNameXpath = cellThreeOne.getContents();
+
+
+
+			Cell cellOneTwo = sheet.getCell(1, 2);
+			objPassword = cellOneTwo.getContents();
+
+			Cell cellTwoTwo = sheet.getCell(2, 2);
+			password = cellTwoTwo.getContents();
+
+			Cell cellThreeTwo = sheet.getCell(3, 2);
+			passwordXpath = cellThreeTwo.getContents();
+
+
+
+			Cell cellOneOneThree = sheet.getCell(1, 3);
+			objLogin= cellOneOneThree.getContents();
+			
+
+			Cell cellThree = sheet.getCell(3, 3);
+			loginXpath = cellThree.getContents();
+			
+			
+			
+			Cell cellOneOneFour = sheet.getCell(1, 4);
+			 objUserProfile = cellOneOneFour.getContents();
+			Cell cellFour = sheet.getCell(3, 4);
+			userProfileID= cellThree.getContents();
+			
+			
+			
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		catch (BiffException e){
+			e.printStackTrace();
+		}
+		driver = new FirefoxDriver();
+		driver.get("https://login.salesforce.com/");
+		reusableMethod obj = new reusableMethod();
+		obj.startReport("LogOut", "/Users/abhaskumar/Desktop/Report/");
+		WebElement userName = driver.findElement(By.xpath(userNameXpath));
+		obj.enterDataForMethodSetByFields(userName, username, objUsername );
+		WebElement passWord = driver.findElement(By.xpath(passwordXpath));
+		obj.enterDataForMethodSetByFields(passWord, password, objPassword );
+		WebElement loginClick = driver.findElement(By.xpath(loginXpath));
+		obj.click(loginClick, objLogin);
+		Thread.sleep(4000);
+		WebElement userProfileClick = driver.findElement(By.id("userNavLabel"));
+		obj.click(userProfileClick, objUserProfile);
+		WebElement logout = driver.findElement(By.linkText("Logout"));
+		obj.click(logout, "logout");
+		
+		
+		
+
+	}
+	
+
+}
--- src/salesforceTestingWithPresentableReports/testCases.java
+++ src/salesforceTestingWithPresentableReports/testCases.java
@@ -3,15 +3,19 @@ package salesforceTestingWithPresentableReports;
 import java.io.IOException;
 
 public class testCases {
+	@SuppressWarnings("static-access")
 	public void testCasesContainer() throws IOException, InterruptedException{
 		reusableMethod obj = new reusableMethod();
-		loginTest.login();
-		loginThenLogout.login();
-		forgotPassword.login();
-		loginFailed.login();
-		Loginandclickuserprofile.login();
-		setting.login();
-		uploadPhoto.login();
+//		loginTest.login();
+//		loginThenLogout.login();
+//		forgotPassword.login();
+//		loginFailed.login();
+//		Loginandclickuserprofile.login();
+//		setting.login();
+//		uploadPhoto.login();
+//		developerConsole.login();
+//		logout.login();
+		createAccount.login();
 		obj.bw.close();
 	}
 }
