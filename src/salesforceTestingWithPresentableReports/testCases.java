6ff98b124fc7e2e2e67aa0a9a6ae01cc28216b5c
abhasrambo <abhasrambo@mail.fresnostate.edu>
2017-08-29 18:19:50 -0700

    10
    10

--- src/salesforceTestingWithPresentableReports/testCases.java
+++ src/salesforceTestingWithPresentableReports/testCases.java
@@ -6,15 +6,15 @@ public class testCases {
 	@SuppressWarnings("static-access")
 	public void testCasesContainer() throws IOException, InterruptedException{
 		reusableMethod obj = new reusableMethod();
-//		loginTest.login();
-//		loginThenLogout.login();
-//		forgotPassword.login();
-//		loginFailed.login();
-//		Loginandclickuserprofile.login();
-//		setting.login();
-//		uploadPhoto.login();
-//		developerConsole.login();
-//		logout.login();
+		loginTest.login();
+		loginThenLogout.login();
+		forgotPassword.login();
+		loginFailed.login();
+		Loginandclickuserprofile.login();
+		setting.login();
+		uploadPhoto.login();
+		developerConsole.login();
+		logout.login();
 		createAccount.login();
 		obj.bw.close();
 	}
